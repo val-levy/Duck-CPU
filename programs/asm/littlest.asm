071303d6c84156a26fa46cf320392836d64ed6ad
Val Levy <146398552+val-levy@users.noreply.github.com>
2025-03-13 16:13:45 -0700

    Add files via upload

---
+++ programs/asm/littlest.asm
@@ -0,0 +1,8 @@
+# Lovingly crafted by the robots of CIS 211
+# 2025-03-07 17:15:09.095081 from programs/mal/littlest.mal
+#
+    LOAD r14,const_7
+   STORE  r14,var_x
+	HALT  r0,r0,r0
+const_7: DATA 7
+var_x: DATA 0
